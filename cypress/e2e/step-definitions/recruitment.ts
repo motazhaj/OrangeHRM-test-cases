2cda518dec8b9d9d44f9f8bea83724da5bd29411
Motaz HM <motazhajm@gmail.com>
2023-11-16 23:15:50 +0200

    Implement the hired successfully test scenario

--- cypress/e2e/step-definitions/recruitment.ts
+++ cypress/e2e/step-definitions/recruitment.ts
@@ -1,9 +1,11 @@
 import { Given, When, Then } from "cypress-cucumber-preprocessor/steps";
 import { LoginPageActions } from "../pageObjects/loginPage/actions";
 import { RecruitmentActions } from "../pageObjects/recruitmentPage/actions";
+import { RecruitmentAssertions } from "../pageObjects/recruitmentPage/assertions";
 
 const loginPageActions = new LoginPageActions();
 const recruitmentActions = new RecruitmentActions();
+const recruitmentAssertions = new RecruitmentAssertions();
 
 Given("the user is on the recruitment page", () => {
   loginPageActions.openLoginPage();
@@ -22,4 +24,34 @@ When("the user adds a new recruit", () => {
   recruitmentActions.email("motaz@test.com");
   recruitmentActions.contactNumber("0599123456");
   recruitmentActions.saveButton();
+});
+
+When("the user shortlists the recruit", () => {
+  recruitmentActions.shortlistButton();
+  recruitmentActions.saveButton();
+});
+
+When("the user schedules an interview with the recruit", () => {
+  recruitmentActions.scheduleInterviewButton();
+  recruitmentActions.interviewInfo();
+  recruitmentActions.saveButton();
+});
+
+When("the user marks the interview as passed", () => {
+  recruitmentActions.markInterviewPassedButton();
+  recruitmentActions.saveButton();
+});
+
+When("the user Offer's the recruit a job", () => {
+  recruitmentActions.offerJobButton();
+  recruitmentActions.saveButton();
+});
+
+When("the user hires the recruit", () => {
+  recruitmentActions.hireButton();
+  recruitmentActions.saveButton();
+});
+
+Then("the recruit should be hired", () => {
+  recruitmentAssertions.statusHired();
 });
