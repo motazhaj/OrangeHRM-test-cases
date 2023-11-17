50f7e34e892485bbf011e98a0031faa4fb7869c4
Motaz HM <motazhajm@gmail.com>
2023-11-17 19:15:06 +0200

    Implement the assertions in the recruitment page assertions file

--- cypress/e2e/pageObjects/recruitmentPage/assertions.ts
+++ cypress/e2e/pageObjects/recruitmentPage/assertions.ts
@@ -1,4 +1,52 @@
 export class RecruitmentAssertions {
+  statusApplicationInitiated() {
+    cy.get(
+      "p.oxd-text.oxd-text--p.oxd-text--subtitle-2[data-v-7b563373]"
+    ).should("contain", "Status: Application Initiated");
+  }
+
+  statusShortlisted() {
+    cy.get(
+      "p.oxd-text.oxd-text--p.oxd-text--subtitle-2[data-v-7b563373]"
+    ).should("contain", "Status: Shortlisted");
+  }
+
+  statusInterviewScheduled() {
+    cy.get(
+      "p.oxd-text.oxd-text--p.oxd-text--subtitle-2[data-v-7b563373]"
+    ).should("contain", "Status: Interview Scheduled");
+  }
+
+  statusInterviewPassed(){
+    cy.get(
+      "p.oxd-text.oxd-text--p.oxd-text--subtitle-2[data-v-7b563373]"
+    ).should("contain", "Status: Interview Passed");
+  }
+
+  statusInterviewFailed() {
+    cy.get(
+      "p.oxd-text.oxd-text--p.oxd-text--subtitle-2[data-v-7b563373]"
+    ).should("contain", "Status: Interview Failed");
+  }
+
+  statusJobOffered() {
+    cy.get(
+      "p.oxd-text.oxd-text--p.oxd-text--subtitle-2[data-v-7b563373]"
+    ).should("contain", "Status: Job Offered");
+  }
+
+  statusJobOfferDeclined() {
+    cy.get(
+      "p.oxd-text.oxd-text--p.oxd-text--subtitle-2[data-v-7b563373]"
+    ).should("contain", "Status: Offer Declined");
+  }
+
+  statusRejected() {
+    cy.get(
+      "p.oxd-text.oxd-text--p.oxd-text--subtitle-2[data-v-7b563373]"
+    ).should("contain", "Status: Rejected");
+  }
+
   statusHired() {
     cy.get(
       "p.oxd-text.oxd-text--p.oxd-text--subtitle-2[data-v-7b563373]"
